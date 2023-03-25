8ac0e4b0ea8542fb7063953a4312cba43421e0f4
Leaced <likluba@mail.de>
2023-03-25 19:19:50 +0100

    Add flag for is_wayland

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -3,7 +3,20 @@
 # If not, do $mod+Shift+c to reload the configuration.
 
 date_formatted=$(date "+%a %d.%m.%Y %H:%M")
-audio_muted=$(pactl get-sink-mute @DEFAULT_SINK@ | awk '{ print $2=="yes" ? "🔇":"🔉" }')
-audio_volume=$(pactl get-sink-volume @DEFAULT_SINK@ | awk '{ print $5 }')
 
-echo $audio_volume $audio_muted "|" $date_formatted
+audio_muted=$(
+	pactl get-sink-mute @DEFAULT_SINK@ |
+	awk '{ print $2=="yes" ? "🔇":"🔉" }')
+audio_volume=$(
+	pactl get-sink-volume @DEFAULT_SINK@ |
+	awk '{ print $5 }')
+
+is_wayland=$(
+	swaymsg -t get_tree |
+	awk '
+		/focused": true/,/shell/ {
+			if($1 == "\"shell\":") print $2=="\"xdg_shell\"," ? "wayland ✔":"✘wayland"
+		}
+	')
+
+echo $is_wayland "|" $audio_volume $audio_muted "|" $date_formatted
